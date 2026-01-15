562c27435d7de70076f9247efb17a542712f869c
DaCong114514qwq <da_cong114514owo@outlook.com>
2026-01-16 00:38:30 +0800

    签名全删了

--- src/main/java/gugu/cong/carpet_gugu_addition/mixins/rule/reIntroduceSkeletonLooting/AbstractSkeletonMixin.java
+++ src/main/java/gugu/cong/carpet_gugu_addition/mixins/rule/reIntroduceSkeletonLooting/AbstractSkeletonMixin.java
@@ -1,23 +1,3 @@
-/*
- * This file is part of the Carpet Dacong Addition project, licensed under the
- * GNU Lesser General Public License v3.0
- *
- * Copyright (C) 2025  Da_Cong and contributors
- *
- * Carpet Dacong Addition is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Carpet Dacong Addition is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Carpet Dacong Addition.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package gugu.cong.carpet_gugu_addition.mixins.rule.reIntroduceSkeletonLooting;
 
 import gugu.cong.carpet_gugu_addition.GUGUSettings;
